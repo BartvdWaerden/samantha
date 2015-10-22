f4e4d724c96c44a81dc5949cc62c972e2499108e
Bart van der Waerden <BartvdWaerden@users.noreply.github.com>
2015-10-22 18:53:10 +0200

    Update rodesk-projectinventarisatie.php

--- rodesk-projectinventarisatie.php
+++ rodesk-projectinventarisatie.php
@@ -19,14 +19,14 @@
             </div>
 
             <div class="o-pullquote u-pullviewport">
-                <p>‘‘Ik heb mijn ontwerpkeuzes goed toe kunnen lichten omdat ik grondig onderzoek heb gedaan’’.</p>
+                <p>‘‘Ik heb mijn ontwerpkeuzes goed toe kunnen lichten omdat ik grondig onderzoek heb gedaan’’</p>
             </div>
 
             <div class="o-section">
                 <h2>Het proces</h2>
                 <p class="c-section__intro">Het proces dat ik doorlopen heb was in het begin kijken hoe ik de projectinventarisatie leuk kon houden voor de gebruiker. Hierbij was het overzicht ook belangrijk, zodat de gebruiker niet het gevoel heeft dat hij of zij een eindeloos lange lijst met vragen in moet vullen.</p>
                 <h3>Onderzoek</h3>
-                <p>Het overzicht is een belangrijk onderdeel van de inventarisatie, omdat de gebruiker redelijk wat vragen in moet vullen. Om de gebruiker een indicatie te geven van hoever hij of zij in de inventarisatie is, heb ik een balk ontworpen voor de voortgang. Omdat de focus ligt op de vragen, is de voortgangsbalk een subtiele witte balk met daarin een indicator met de voortgang. Ook hier is de herkenning van Rodesk belangrijk, en heb ik hun huisstijl verwerkt in de ontwerpen.</p>
+                <p>Het overzicht is een belangrijk onderdeel van de inventarisatie, omdat de gebruiker redelijk wat vragen in moet vullen. Om de gebruiker een indicatie te geven van hoever hij of zij in de inventarisatie is, heb ik een balk ontworpen voor de voortgang. Omdat de focus ligt op de vragen, is de voortgangsbalk een subtiele witte balk met daarin een indicator met de voortgang. Ook hier is de herkenning van Rodesk belangrijk en heb ik hun huisstijl verwerkt in de ontwerpen.</p>
                 <h3>Badges</h3>
                 <p>Om de inventarisatie leuk te maken, krijgt de gebruiker een badge te wanneer hij of zij halverwege de inventarisatie is. Zo worden de gebruikers gemotiveerd om de inventarisatie af te maken. Daarnaast verdienen ze een badge als ze de inventarisatie hebben afgerond als een soort beloning voor het invullen. Omdat het een badge is die de gebruiker van Rodesk krijgt, was het belangrijk dat deze ook in Rodesk stijl ontworpen werd.</p>
             </div>
@@ -40,7 +40,7 @@
 
             <div class="o-section">
                 <h2>Het resultaat</h2>
-                <p class="c-section__intro">Uiteindelijk is dit het resultaat geworden, een responsive website voor de projectinventarisatie van Rodesk. De inventarisatie is persoonlijk en de gebruiker krijgt badges voor het invullen. Op deze manier wordt er van de inventarisatie die toch wel lastig en een beetje saai is iets leuks gemaakt.</p>
+                <p class="c-section__intro">Uiteindelijk is dit het resultaat geworden, een responsive website voor de projectinventarisatie van Rodesk. De inventarisatie is persoonlijk en de gebruiker krijgt badges voor het invullen. Op deze manier wordt er van de inventarisatie, die toch wel lastig en een beetje saai kan zijn, iets leuks gemaakt.</p>
 
                 <div class="o-grid u-space-leader-xl">
                     <div class="o-grid-cell u-size1of2--medium u-size1of3--large u-pull-left">
@@ -74,7 +74,7 @@
                     <div class="o-grid-cell u-size1of2--medium u-size1of3--large u-pull-left">
                         <div class="o-section__content">
                             <h4>Overzicht</h4>
-                            <p>Overzicht voor de gebruiker om te zien hoe ver hij of zij is in de inventarisatie, en hoeveel categoriën er nog ingevuld moeten worden. Deze voortgangsbalk staat fixed aan de menubalk vast om de gebruiker altijd overzicht te bieden.</p>
+                            <p>Overzicht voor de gebruiker om te zien hoe ver hij of zij is in de inventarisatie en hoeveel categoriën er nog ingevuld moeten worden. Deze voortgangsbalk staat fixed aan de menubalk vast om de gebruiker altijd overzicht te bieden.</p>
                         </div>
                     </div>
                     <div class="o-grid-cell u-size1of2--medium u-size2of3--large u-pull-right">
